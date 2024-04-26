d4ed450003bf8595da50922fc046f8e39b36d485
yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>
2024-04-26 23:04:35 +0530

    Update play.py
    Signed-off-by: yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -17,11 +17,41 @@ def track_markup(_, videoid, user_id, channel, fplay):
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
             ),
         ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            )
+        ],
     ]
     return buttons
 
 
-
+def stream_markup_timer(_, chat_id, played, dur):
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    umm = math.floor(percentage)
+    if 0 < umm <= 10:
+        bar = "■—————————"
+    elif 10 < umm < 20:
+        bar = "—•————————"
+    elif 20 <= umm < 30:
+        bar = "——°———————"
+    elif 30 <= umm < 40:
+        bar = "———☆——————"
+    elif 40 <= umm < 50:
+        bar = "————¤—————"
+    elif 50 <= umm < 60:
+        bar = "—————¿————"
+    elif 60 <= umm < 70:
+        bar = "——————♧———"
+    elif 70 <= umm < 80:
+        bar = "———————◇——"
+    elif 80 <= umm < 95:
+        bar = "————————♤—"
+    else:
+        bar = "—————————♡"
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
